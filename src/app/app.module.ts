dfa0d20d9d9ad9e0d08faec6df31ca8c5889c464
Lu Rumbo <lurumbo18@gmail.com>
2020-02-02 22:44:34 -0300

    refactor - adds card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { SearchComponent } from './components/search/search.component';
 import { ArtistComponent } from './components/artist/artist.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { NoimagePipe } from './pipes/noimage.pipe';
+import { CardsComponent } from './components/cards/cards.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { NoimagePipe } from './pipes/noimage.pipe';
     SearchComponent,
     ArtistComponent,
     NavbarComponent,
-    NoimagePipe
+    NoimagePipe,
+    CardsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/cards/cards.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-cards',
+  templateUrl: './cards.component.html',
+  styles: []
+})
+export class CardsComponent implements OnInit {
+
+  @Input() items:any[] = [];
+
+  constructor() { 
+    console.log('items', this.items)
+  }
+
+  ngOnInit() {
+  }
+
+}
