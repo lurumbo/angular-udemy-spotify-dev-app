22637aedc572cda982c215146fb54ab1a9857e35
Lu Rumbo <lurumbo18@gmail.com>
2020-02-02 21:13:37 -0300

    WIP - adds artists search component

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SpotifyService } from '../../services/spotify.service';
 
 @Component({
   selector: 'app-search',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  artists:any[] = [];
+
+  constructor(private spotifyService:SpotifyService) { }
 
   ngOnInit() {
+  }
+
+  search(term:string) {
+    console.log('text searched', term);
+    this.spotifyService.getArtist(term)
+      .subscribe( (artists:any) => {
+        console.log(artists.artists.items)
+        this.artists = artists.artists.items;
+      });
+
   }
 
 }
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -6,7 +6,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class SpotifyService {
 
-  token:string = 'BQAtlY11i7brtUMtWbI5BRaM0wuAnilveEJUMrRGDOkYCpsXEIKETorZjXDSlgUTi5FD1pHEXaRBKkgU5_A';
+  token:string = 'BQDQ1WXpxBiNK0wpsKitFkdhpga6IfC22v4LU9rVnEOer0qj2Bo2LaBmm3Bf0ORlKHscxwWO1u0hOTjJmUQ';
   headers = new HttpHeaders({
     'Authorization': 'Bearer ' + this.token
   });
@@ -16,6 +16,11 @@ export class SpotifyService {
   getNewReleases() {
     const headers = this.headers;
     return this.http.get('https://api.spotify.com/v1/browse/new-releases', { headers });
+  }
+
+  getArtist(searchTerm:string) {
+    const headers = this.headers;
+    return this.http.get(`https://api.spotify.com/v1/search?query=${searchTerm}&type=artist&offset=0&limit=15`, { headers });
   }
 
 }
